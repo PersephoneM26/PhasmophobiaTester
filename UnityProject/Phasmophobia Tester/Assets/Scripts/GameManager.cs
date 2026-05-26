acbf4df04f4bc16b806ac8ac6c8f32051efeac26
persephone <persephonemurray26@gmail.com>
2026-05-26 15:35:43 -0700

    added ghost footstep sounds based on speed

--- UnityProject/Phasmophobia Tester/Assets/Scripts/GameManager.cs
+++ UnityProject/Phasmophobia Tester/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,13 +6,13 @@ public class GameManager : MonoBehaviour
 {
     // Serialized ghost variables
     [SerializeField] private List<Ghost> ghostTypes = new();
-    [SerializeField] private bool isMale;
     [SerializeField] private List<string> ghostNamesMale, ghostNamesFemale;
     [SerializeField] private List<GameObject> ghostModelsMale = new(), ghostModelsFemale = new();
+    [SerializeField] private EventReference footstep;
 
     // Internal ghost variables
-    private List<Ghost> ghostTypesMale = new(), ghostTypesFemale = new();
-    private Ghost ghost;
+    private bool isMale;
+    private Ghost ghostPrefab, ghost;
     private GameObject ghostModel;
 
     // Ghost properties
@@ -22,16 +23,17 @@ public class GameManager : MonoBehaviour
 
     //Misc properties
     public Canvas Canvas => canvas;
+    public EventReference Footstep => footstep;
 
     private void Start()
     {
         //setup ghost variables
         isMale = Random.value > 0.5f;
         List<Ghost> genderedGhostTypes = GetGenderedGhostList(isMale, ghostTypes);
-        ghost = genderedGhostTypes[Random.Range(0, genderedGhostTypes.Count)];
+        ghostPrefab = genderedGhostTypes[Random.Range(0, genderedGhostTypes.Count)];
         if(isMale) ghostModel = ghostModelsMale[Random.Range(0, ghostModelsMale.Count)];
         else ghostModel = ghostModelsFemale[Random.Range(0, ghostModelsFemale.Count)];
-        Instantiate(ghost);
+        ghost = Instantiate(ghostPrefab);
     }
 
     private List<Ghost> GetGenderedGhostList(bool isMale, List<Ghost> allGhosts)
@@ -51,13 +53,13 @@ public class GameManager : MonoBehaviour
         return allGhosts;
     }
 
-    private void PlaySounds()
+    public void PlaySounds()
     {
-
+        ghost.PlayFootsteps();
     }
 
-    private void StopSounds()
+    public void StopSounds()
     {
-
+        ghost.StopFootsteps();
     }
 }
--- UnityProject/Phasmophobia Tester/Assets/Scripts/Ghost.cs
+++ UnityProject/Phasmophobia Tester/Assets/Scripts/Ghost.cs
@@ -19,6 +19,7 @@ public class Ghost : MonoBehaviour
 
     protected Canvas canvas;
     protected GameObject ghostModel;
+    protected float currentSpeed, currentStepsPerSecond;
 
     public ghostGender GhostGender => ghostGender;
 
@@ -28,6 +29,8 @@ public class Ghost : MonoBehaviour
         canvas = gameManager.Canvas;
         ghostModel = gameManager.GhostModel;
         Instantiate(ghostModel, canvas.transform);
+        currentSpeed = walkSpeed;
+        currentStepsPerSecond = SpeedToStepsPerSecond(currentSpeed);
     }
 
 
@@ -39,5 +42,25 @@ public class Ghost : MonoBehaviour
     protected virtual void LoseLOS()
     {
 
+    }
+
+    public void PlayFootsteps()
+    {
+        InvokeRepeating(nameof(Step), 0, currentStepsPerSecond);
+    }
+
+    public void StopFootsteps()
+    {
+        CancelInvoke();
+    }
+
+    private void Step()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(gameManager.Footstep);
+    }
+
+    private float SpeedToStepsPerSecond(float speed)
+    {
+        return 60f / (5.3847f * (Mathf.Pow(speed, 2f)) + 46.3881f * speed + 22.9986f);
     }
 }
